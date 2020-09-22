a8ac9ea4b366658f31abc5f45c7e4e4ec31c47cc
Campfiree <54428065+Campfiree@users.noreply.github.com>
2020-09-21 17:28:20 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Search</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <form>
+            <input type="text" placeholder="Enter a zip code">
+        </form>
+    </body>
+        
+</html>
+
