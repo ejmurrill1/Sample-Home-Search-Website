6a82298779000a4f53712cf586a35f40a8de445c
Eric Murrill <71233965+ejmurrill1@users.noreply.github.com>
2020-09-29 13:31:46 -0700

    Create bootstrap style.php
    work in progress

---
+++ bootstrap style.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="row">
+<div class="col-sm" style="background-color:yellow;">25%</div>
+<div class="col-sm" style="background-color:orange;">25%</div>
+<div class="col-sm" style="background-color:yellow;">25%</div>
+<div class="col-sm" style="background-color:orange;">25%</div>
+</div>
+<br>
+
+<div class="row">
+<div class="col" style="background-color:yellow;">25%</div>
+<div class="col" style="background-color:orange;">25%</div>
+<div class="col" style="background-color:yellow;">25%</div>
+<div class="col" style="background-color:orange;">25%</div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
