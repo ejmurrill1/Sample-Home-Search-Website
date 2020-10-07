fec1ae2b40d2d0661fdecbc31be89249fc5217bc
Campfiree <54428065+Campfiree@users.noreply.github.com>
2020-10-06 22:08:16 -0700

    Filter Options added for Merhdad's variables

--- houseListings.php
+++ houseListings.php
@@ -10,7 +10,12 @@ include 'database.php';
 
 <div class="houseList">
   <form action="houseListings.php" method="POST">
-    <input type="text" name="search" placeholder="Search" aria-label="Search">
+    <input type="text" name="search" placeholder="Search" aria-label="Search"
+           <?php 
+               if (!empty($_POST['search'])){
+                   echo 'value=\'' . $_POST['search'] . '\'';
+               }
+               ?>>
     <button type="submit" name="submit-search">Search</button>
 
      <select name="inputBed">
@@ -42,50 +47,223 @@ include 'database.php';
     <input type="text" placeholder="Min SqFt" name="minSqFt">
     <input type="text" placeholder="Max SqFt" name ="maxSqFt">
 
-<div class="form-row">
-    <input type="text" placeholder="Min Population" name="minPopulation">
-    <input type="text" placeholder="Max Population" name ="maxPopulation">
+    <div class="form-row">
+        <label for="minPopulation">Minimum City Population</label>
+        <input type="text" placeholder="Min Population" name="minPopulation" 
+               <?php 
+               if (!empty($_POST['minPopulation'])){
+                   echo 'value=\'' . $_POST['minPopulation'] . '\'';
+               }
+               ?>>
+        <label for="maxPopulation">Maximum City Population</label>
+        <input type="text" placeholder="Max Population" name ="maxPopulation"
+               <?php 
+               if (!empty($_POST['maxPopulation'])){
+                   echo 'value=\'' . $_POST['maxPopulation'] . '\'';
+               }
+               ?>>
+        <label for="minMedianAge">Minimum Median Age</label>
+        <input type="text" placeholder="Min Median Age" name="minMedianAge"
+               <?php 
+               if (!empty($_POST['minMedianAge'])){
+                   echo 'value=\'' . $_POST['minMedianAge'] . '\'';
+               }
+               ?>>
+        <label for="maxMedianAge">Maximum Median Age</label>
+        <input type="text" placeholder="Max Median Age" name ="maxMedianAge"
+               <?php 
+               if (!empty($_POST['maxMedianAge'])){
+                   echo 'value=\'' . $_POST['maxMedianAge'] . '\'';
+               }
+               ?>>
+        <label for="minMedianHouseholdIncome">2020 Minimum Median Household Income</label>       
+        <input type="text" placeholder="Min Median Household Income" name="minMedianHouseholdIncome"
+               <?php 
+               if (!empty($_POST['minMedianHousholdIncome'])){
+                   echo 'value=\'' . $_POST['minMedianHousholdIncome'] . '\'';
+               }
+               ?>>
+        <label for="maxMedianHouseholdIncome">2020 Maximum Median Household Income</label>
+        <input type="text" placeholder="Max Median Household Income" name ="maxMedianHouseholdIncome"
+               <?php 
+               if (!empty($_POST['maxMedianHousholdIncome'])){
+                   echo 'value=\'' . $_POST['maxMedianHousholdIncome'] . '\'';
+               }
+               ?>>
+        
+        <label for="minUnemploymentRate">2020 Minimum Unemployment Rate</label>
+        <input type="text" placeholder="Min Unemployment Rate" name="minUnemploymentRate" 
+               <?php 
+               if (!empty($_POST['minUnemploymentRate'])){
+                   echo 'value=\'' . $_POST['minUnemploymentRate'] . '\'';
+               }
+               ?>>
+        <label for="maxUnemploymentRate">2020 Maximum Unemployment Rate</label>
+        <input type="text" placeholder="Max Unemployment Rate" name ="maxUnemploymentRate"
+               <?php 
+               if (!empty($_POST['maxUnemploymentRate'])){
+                   echo 'value=\'' . $_POST['maxUnemploymentRate'] . '\'';
+               }
+               ?>>
+        <label>2020 Minimum Property Crime Index</label>
+        <input type="text" placeholder="Min Crime Index" name="minCrimeIndex"
+               <?php 
+               if (!empty($_POST['minCrimeIndex'])){
+                   echo 'value=\'' . $_POST['minCrimeIndex'] . '\'';
+               }
+               ?>>
+        <label>2020 Maximum Property Crime Index</label>
+        <input type="text" placeholder="Max Crime Index" name ="maxCrimeIndex"
+               <?php 
+               if (!empty($_POST['maxCrimeIndex'])){
+                   echo 'value=\'' . $_POST['maxCrimeIndex'] . '\'';
+               }
+               ?>>
+        <label>2020-2025 Growth Rate: Population</label>
+        <input type="text" placeholder="Min Growth Rate" name="minPopulationGrowth"
+               <?php 
+               if (!empty($_POST['minPopulationGrowth'])){
+                   echo 'value=\'' . $_POST['minPopulationGrowth'] . '\'';
+               }
+               ?>>
+        <input type="text" placeholder="Max Growth Rate" name ="maxPopulationGrowth"
+               <?php 
+               if (!empty($_POST['maxPopulationGrowth'])){
+                   echo 'value=\'' . $_POST['maxPopulationGrowth'] . '\'';
+               }
+               ?>>
+        <label for="minBachelorsDegree">2020 Population Age 25+: Bachelor's Degree</label>      
+        <input type="text" placeholder="Min Bachelors Degree" name="minBachelorsDegree"
+               <?php 
+               if (!empty($_POST['minBachelorsDegree'])){
+                   echo 'value=\'' . $_POST['minBachelorsDegree'] . '\'';
+               }
+               ?>>
+        <label for="minTotalHousingUnit">2020 Minimum Total Housing Units</label>
+        <input type="text" placeholder="Min Total Housing Unit" name="minTotalHousingUnit"
+               <?php 
+               if (!empty($_POST['minTotalHousingUnit'])){
+                   echo 'value=\'' . $_POST['minTotalHousingUnit'] . '\'';
+               }
+               ?>>
+        <label for="maxTotalHousingUnit">2020 Maximum Total Housing Units</label>
+        <input type="text" placeholder="Max Total Housing Unit" name ="maxTotalHousingUnit"
+               <?php 
+               if (!empty($_POST['maxTotalHousingUnit'])){
+                   echo 'value=\'' . $_POST['maxTotalHousingUnit'] . '\'';
+               }
+               ?>>
+        <label>2020 Ownder Occupied Housing Units</label>
+        <input type="text" placeholder="Min Owner Occupied HUs" name="minOwnerOccupiedHUs"
+               <?php 
+               if (!empty($_POST['minOwnerOccupiedHUs'])){
+                   echo 'value=\'' . $_POST['minOwnerOccupiedHUs'] . '\'';
+               }
+               ?>>
+        <input type="text" placeholder="Max Owner Occupied HUs" name ="maxOwnerOccupiedHUs"
+               <?php 
+               if (!empty($_POST['maxOwnerOccupiedHUs'])){
+                   echo 'value=\'' . $_POST['maxOwnerOccupiedHUs'] . '\'';
+               }
+               ?>>
 
-    <input type="text" placeholder="Min Median Age" name="minMedianAge">
-    <input type="text" placeholder="Max Median Age" name ="maxMedianAge">
-
-    <input type="text" placeholder="Min Median Household Income" name="minMedianHouseholdIncome">
-    <input type="text" placeholder="Max Median Household Income" name ="maxMedianHouseholdIncome">
-
-    <input type="text" placeholder="Min Unemployment Rate" name="minUnemploymentRate">
-    <input type="text" placeholder="Max Unemployment Rate" name ="maxUnemployment Rate">
-
-    <input type="text" placeholder="Min Crime Index" name="minCrimeIndex">
-    <input type="text" placeholder="Max Crime Index" name ="maxCrimeIndex">
-
-    <input type="text" placeholder="Min Growth Population" name="minGrowthPopulation">
-    <input type="text" placeholder="Max Growth Population" name ="maxGrowthPopulation">
-
-    <input type="text" placeholder="Min Bachelors Degree" name="minBachelorsDegree">
-    <input type="text" placeholder="Max Bachelors Degree" name ="maxBachelorsDegree">
-
-    <input type="text" placeholder="Min Total Housing Unit" name="minTotalHousingUnit">
-    <input type="text" placeholder="Max Total Housing Unit" name ="maxTotalHousingUnit">
-
-    <input type="text" placeholder="Min Owner Occupied HUs" name="minOwnerOccupiedHUs">
-    <input type="text" placeholder="Max Owner Occupied HUs" name ="maxOwnerOccupiedHUs">
-
-    <input type="text" placeholder="Min Air Quality" name="minAirQuality">
-    <input type="text" placeholder="Max Air Quality" name ="maxAirQuality">
-</div>
-
-
-
-
+        <label for="airQuality">Minimum Air Quality</label>
+        <input type="text" placeholder="Enter index 1-100" name="airQuality" 
+               <?php 
+               if (!empty($_POST['airQuality'])){
+                   echo 'value=\'' . $_POST['airQuality'] . '\'';
+               }
+               ?>>
+    </div>
   </form>
+    
   <?php
+  $minPop = filter_input(INPUT_POST, 'minPopulation');
+  $maxPop = filter_input(INPUT_POST, 'maxPopulation');
+  $minMedAge = filter_input(INPUT_POST, 'minMedianAge');
+  $maxMedAge = filter_input(INPUT_POST, 'maxMedianAge');
+  $minMedHI = filter_input(INPUT_POST, 'minMedianHouseholdIncome');
+  $maxMedHI = filter_input(INPUT_POST, 'maxMedianHouseholdIncome');
+  $minUR = filter_input(INPUT_POST, 'minUnemploymentRate');
+  $maxUR = filter_input(INPUT_POST, 'maxUnemploymentRate');
+  $minCI = filter_input(INPUT_POST, 'minCrimeIndex');
+  $maxCI = filter_input(INPUT_POST, 'maxCrimeIndex');
+  $minPopGR = filter_input(INPUT_POST, 'minPopulationGrowth');
+  $maxPopGR = filter_input(INPUT_POST, 'maxPopulationGrowth');
+  $minBD = filter_input(INPUT_POST, 'minBachelorsDegree');
+  $minTHU = filter_input(INPUT_POST, 'minTotalHousingUnit');
+  $maxTHU = filter_input(INPUT_POST, 'maxTotalHousingUnit');
+  $minOOHU = filter_input(INPUT_POST, 'minOwnerOccupiedHUs');
+  $maxOOHU = filter_input(INPUT_POST, 'maxOwnerOccupiedHUs');
+  $airQuality = filter_input(INPUT_POST, 'airQuality');
+  
+  //whichever filter option is set is added to an array that has all the conditions
+  $conditions = array();
+  if (!empty($minPop)) {
+      $conditions[] = "population >= $minPop";
+  }
+  if (!empty($maxPop)) {
+      $conditions[] = "population <= $maxPop";
+  }
+  if (!empty($minMedAge)) {
+      $conditions[] = "MedianAge >= $minMedAge";
+  }
+  if (!empty($maxMedAge)) {
+      $conditions[] = "MedianAge <= $maxMedAge";
+  }
+  if (!empty($minMedHI)) {
+      $conditions[] = "MedianHouseholdIncome >= $minMedHI";
+  }
+  if (!empty($maxMedHI)) {
+      $conditions[] = "MedianHouseholdIncome <= $maxMedHI";
+  }
+  if (!empty($minUR)) {
+      $conditions[] = "UnemploymentRate >= $minUR";
+  }
+  if (!empty($maxUR)) {
+      $conditions[] = "UnemploymentRate <= $maxUR";
+  }
+  if (!empty($minCI)) {
+      $conditions[] = "PropertyCrimeIndex >= $minCI";
+  }
+  if (!empty($maxCI)) {
+      $conditions[] = "PropertyCrimeIndex <= $maxCI";
+  }
+  if (!empty($minPopGR)) {
+      $conditions[] = "PopulationGrowthRate >= $minPopGR";
+  }
+  if (!empty($maxPopGR)) {
+      $conditions[] = "PopulationGrowthRate <= $maxPopGR";
+  }
+  if (!empty($minBD)) {
+      $conditions[] = "BachelorDegree >= $minBD";
+  }
+  if (!empty($minTHU)) {
+      $conditions[] = "TotalHousingUnits >= $minTHU";
+  }
+  if (!empty($maxTHU)) {
+      $conditions[] = "TotalHousingUnits <= $maxTHU";
+  }
+  if (!empty($minOOHU)) {
+      $conditions[] = "OwnerOccupiedHUs >= $minOOHU";
+  }
+  if (!empty($maxOOHU)) {
+      $conditions[] = "OwnerOccupiedHUs <= $maxOOHU";
+  }
+  if(!empty($airQuality)) {
+      $conditions[] = "airquality >= $airQuality";
+  }
 
   if(isset($_POST['submit-search']))
     {
     $search = mysqli_real_escape_string($conn, $_POST['search']);
-    $bed =  mysqli_real_escape_string($conn, $_POST['inputBed']);
-    $bath =  mysqli_real_escape_string($conn, $_POST['inputBath']);
-    $sql = "SELECT * FROM cis4290 WHERE address = '$search' OR state='$search' OR zipcode = '$search' OR city = '$search' OR numbed >= '$bed' AND numbath >='$bath'";
+    $sql = "SELECT * FROM combinedhomes WHERE (address = '$search' OR state= '$search' OR zipcode = '$search' OR city = '$search')";
+    //append 'and' conditions to sql statement
+    if(count($conditions) === 1){
+        $sql .= ' AND ' . $conditions[0];
+    } else if(count($conditions) > 1) {
+        $sql .= ' AND ' . implode(' AND ', $conditions);
+    }
     $result = mysqli_query($conn, $sql);
     $queryResult=mysqli_num_rows($result);
 
@@ -106,9 +284,9 @@ include 'database.php';
     else{
       echo "There are no results matching your search!";
     }
+    $conn->close();
   }
   ?>
 </div>
-
 </body>
 </html>
