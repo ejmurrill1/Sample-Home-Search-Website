9d539f2ce0668e14f361e5e71486467bc42d78d4
Eric Murrill <71233965+ejmurrill1@users.noreply.github.com>
2020-10-11 16:06:09 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,38 @@
 <head>
 
   <link rel="stylesheet" href="style.css">
-
   <div id="navbar">
    <a href="index.php">Home</a>
    <a href="loginPage.php">Login</a>
   </div>
 </head>
-
 <body>
-
-
     <form action="houseListings.php" method="POST">
       <input type="text" name="search" placeholder="Search">
       <button type="submit" name="submit-search">Search</button>
     </form>
+
+    <div class="jumbotron text-center">
+  <h1>Home Property Search</h1>
+  <p>Resize this responsive page to see the effect!</p>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor..</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor..</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>
+      <p>Lorem ipsum dolor..</p>
+    </div>
+  </div>
+</div>  
 </body>
 </html>
 
