f52f8da5b139ea63b547a9d8128619c98c6e1d73
Campfiree <54428065+Campfiree@users.noreply.github.com>
2020-10-24 05:36:01 -0700

    Added card css, made cards clickable to go to details page

--- houseListings.php
+++ houseListings.php
@@ -347,9 +347,9 @@ session_start();
         }
     }
     if ($inputOrder !== null && $inputOrder !== 'Any' && $orderType !== null && $orderType !== 'Any'){
-        $sql = "SELECT * FROM cis4290 $where $order $orderby";
+        $sql = "SELECT * FROM combinedhomes $where $order $orderby";
     } else {
-        $sql = "SELECT * FROM cis4290 $where";
+        $sql = "SELECT * FROM combinedhomes $where";
     }
     $result = mysqli_query($conn, $sql);
     $connectsqli = $conn->query($sql);
@@ -358,19 +358,27 @@ session_start();
         <p style="color: white">Sorting by <?php echo $inputOrder . " " . $orderType . "ing"; ?></p>  
     <?php     
     }
-    
+    ?> 
+      
+        <div class="container mt-2">
+        <div class="row">
+    <?php
     if(!empty($connectsqli) && $connectsqli->num_rows > 0){
       while($row = mysqli_fetch_assoc($result)) { ?>
-        <div class='card mb-3' style='width: 18rem;'>
-        <a href=detailsPage.php?id=[address]><img class='card-img-top' src="<?php echo $row['image-src'] ?>" alt='Avatar' style='width:100%'></a>
-            <div class='container'>
-                <h3><?php echo $row['address'] ?></h3>
-                <p><?php echo $row['city'] . ", " . $row['state'] . " " . $row['zipcode'] ?></p>
-                <p><?php echo $row['price'] ?></p>
-                <p><?php echo $row['numbed'] . " bd " . $row['numbath'] . " bth " . $row['squarefootage'] . " SqFt" ?></p>
-	  <p><a href=detailsPage.php?id=$row[address]>Details</a></p>
+        <div class="col-md-4 col-sm-6">
+            <div class="card card-block" style="width: 18rem;">
+                <a href="detailsPage.php?id=<?php echo $row['address'] ?>" style="text-decoration: none; color: black;">
+                <img class='card-img-top' src="<?php echo $row['image-src'] ?>" alt='Not Found' onerror=this.src="https://d3bmimpiifbojs.cloudfront.net/wp-content/uploads/2017/04/no-image-image-1.png?x83809">
+                    <div class="card-body">
+                        <h3 class="card-title"><?php echo $row['address'] ?></h3>
+                        <p><?php echo $row['city'] . ", " . $row['state'] . " " . $row['zipcode'] ?></p>
+                        <p><?php echo $row['price'] ?></p>
+                        <p><?php echo $row['numbed'] . " bd " . $row['numbath'] . " bth " . $row['squarefootage'] . " SqFt" ?></p>
+                    </div>
+                </a>
             </div>
         </div>
+
     <?php
       }
     }
@@ -380,8 +388,10 @@ session_start();
     }
   $conn->close();
 ?>
-
         </div>
+        </div>
+   
+        
 		
     </div>
 </body>
