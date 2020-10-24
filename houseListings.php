fe3c23c5bda62f4394edbc077cdf8a1e6eb5b502
Campfiree <54428065+Campfiree@users.noreply.github.com>
2020-10-24 05:47:32 -0700

    updated css for houseListings

--- houseListings.php
+++ houseListings.php
@@ -365,8 +365,8 @@ session_start();
     <?php
     if(!empty($connectsqli) && $connectsqli->num_rows > 0){
       while($row = mysqli_fetch_assoc($result)) { ?>
-        <div class="col-md-4 col-sm-6">
-            <div class="card card-block" style="width: 18rem;">
+        <div class="col-md-3 col-sm-6">
+            <div class="card card-block">
                 <a href="detailsPage.php?id=<?php echo $row['address'] ?>" style="text-decoration: none; color: black;">
                 <img class='card-img-top' src="<?php echo $row['image-src'] ?>" alt='Not Found' onerror=this.src="https://d3bmimpiifbojs.cloudfront.net/wp-content/uploads/2017/04/no-image-image-1.png?x83809">
                     <div class="card-body">
