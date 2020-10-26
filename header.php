b83a80dbfba814184eb0b896159f04b2286b14f6
eVLChirp <ashawut.tran@gmail.com>
2020-10-26 13:15:39 -0700

    Update header.php

--- header.php
+++ header.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
 <html>
-<!-- the head section -->
-<head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<!-- the body section -->
-<body>
-    <div id="navbar">
-        <a><img src="img/logo.png" alt="Logo"></a>  
-        <a href="index.php">Home</a>
-        <a href="loginPage.php">Login</a>
-        <a href="registrationPage.php">Registration</a>
-    </div>
-</body>
+    <!-- the head section -->
+    <header>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="style.css">
+        
+        <div id="navbar">
+            <a><img src="img/logo.png" alt="Logo"></a>  
+            <a href="index.php">Home</a>
+            <a href="loginPage.php">Login</a>
+            <a href="registrationPage.php">Registration</a>
+        </div>
+    </header>
