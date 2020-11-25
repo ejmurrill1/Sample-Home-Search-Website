257b5975acab5714602f7791a3cfb8d2d54fad04
Eric Murrill <71233965+ejmurrill1@users.noreply.github.com>
2020-11-24 22:17:01 -0800

    Update registrationPage.php

--- registrationPage.php
+++ registrationPage.php
@@ -25,12 +25,12 @@ if (! empty($_POST["signup-btn"])) {
 <TITLE>Macro Homes - Registration</TITLE>
 <link href="assets/css/phppot-style.css" type="text/css"
 	rel="stylesheet" />
-<link href="assets/css/user-registration.css" type="text/css"
+<link href="user-registration.css" type="text/css"
 	rel="stylesheet" />
 <script src="vendor/jquery/jquery-3.3.1.js" type="text/javascript"></script>
 </HEAD>
 
-<body>
+<BODY>
 
 <div class="phppot-container">
 		<div class="sign-up-container">
